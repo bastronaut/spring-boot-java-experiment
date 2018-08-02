6abfb5eb4e75051da61748cc69621e687637eb2e
bastronaut <btsijtsma@gmail.com>
2018-08-03 00:01:39 +0200

    Spring Boot initializr and project setup

---
+++ src/main/java/com/bastronaut/springbootjavaexperiment/SpringBootJavaExperimentApplication.java
@@ -0,0 +1,12 @@
+package com.bastronaut.springbootjavaexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavaExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJavaExperimentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bastronaut/springbootjavaexperiment/SpringBootJavaExperimentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bastronaut.springbootjavaexperiment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJavaExperimentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
